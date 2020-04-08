61367d4b0415818d21da310fcec835c08c3a6f19
gabriel <gfc@ic.ufal.br>
2020-04-08 16:48:12 -0300

    some changes

--- imc_calc/lib/main.dart
+++ imc_calc/lib/main.dart
@@ -1,8 +1,62 @@
+import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 
+void main() => runApp(MaterialApp(home: Home()));
 
-void main() => runApp(MaterialApp(
-  home:Scaffold(
-    appBar: AppBar(),
-  )
-));
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final TextEditingController heightController = TextEditingController();
+  final TextEditingController weightController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        centerTitle: true,
+        title: Text(
+          "Calculadora de IMC",
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.refresh), onPressed: () {})
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Expanded(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.android,
+                  color: Colors.green,
+                  size: 80,
+                ),
+                TextField(
+                  controller: heightController,
+                  cursorColor: Colors.green,
+                  decoration: InputDecoration(
+                      labelText: "Sua altura (cm):",
+                      labelStyle: TextStyle(color: Colors.green, fontSize: 25)),
+                ),
+                Container(height: 20),
+                TextField(
+                  controller: weightController,
+                  cursorColor: Colors.green,
+                  decoration: InputDecoration(
+                      labelText: "Seu peso (Kg):",
+                      labelStyle: TextStyle(color: Colors.green, fontSize: 25)),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
